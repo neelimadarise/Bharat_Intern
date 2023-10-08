f1b1867f4ccfb286a604ccf008195ce99d10d713
neelimadarise <128125396+neelimadarise@users.noreply.github.com>
2023-10-08 15:43:20 +0530

    Add files via upload

---
+++ qestion1.java
@@ -0,0 +1,27 @@
+package com.example.quiz1;
+
+public class qestion1 {
+    public static  String question[] = {
+            "Who is the first Prime Minister of India? ",
+            "What is the capital of Andhra Pradesh?",
+            "What is the full form of GST?",
+            "which of the following is a leap year?",
+            "How many countries are in the world?"
+
+    };
+    public static  String choices[][] = {
+            {"Ramnath Kovind", "Jawaharlal Nehru", "Indira Gandhi", "Amith Sha"},
+            {"Kurnool", "Vizag", "Amaravathi", "No Capital"},
+            {"Government Service Tax", "Goods Service Travel", "Goods & Service Tax", "Government Supply Tax"},
+            {"2022", "2025", "2026", "2024"},
+            {"192", "193", "194", "195"}
+    };
+    public static  String correctAnswers[] = {
+            "Jawaharlal Nehru",
+            "No Capital",
+            "Goods & Service Tax",
+            "2024",
+            "195"
+    };
+
+}
